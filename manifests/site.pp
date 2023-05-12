4cfcac4cd8e030558ff8d5f866c1f70fff4fb855
Xavier Almora <6189739@bdeb.qc.ca>
2023-05-11 22:56:45 -0400

    Dele5te content on site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a readme file',
-    owner => 'root',
-  }
-}
-node 'master.puppet.vm' {
-  include role::master_server
+
 }
